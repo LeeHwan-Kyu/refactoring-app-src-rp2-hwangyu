d6e96ab1d530400faa476f8e5ca4678377c903be
l-hwangyu <rlxk2279@gmail.com>
2025-11-10 17:33:30 +0900

    add: ConstantMsg.java, ConstantValue.java 追加 狙い：可読性と保守性の向上 定数クラスを追加し、マジックナンバーを定数化した。

---
+++ refactoring_app_hwangyu/src/jp/co/sss/crud/util/ConstantValue.java
@@ -0,0 +1,13 @@
+package jp.co.sss.crud.util;
+
+/**
+ * 設定値をまとめたクラス(SQL関連)
+ *
+ * @author System Shared
+ *
+ */
+public class ConstantValue {
+    public static final int MENU_ALL = 1;
+    public static final int MENU_FIND_BY_ID = 2;
+    public static final int MENU_EXIT = 9;
+}
